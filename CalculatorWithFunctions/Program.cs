e83c57cbce5ecad50a6e6b83e91ef85f18a56178
Roman Gavrilov <roman.gavrilov@tktk.eegit config --global user.email roman.gavrilov@tktk.ee>
2024-12-11 22:17:33 +0200

    Week7

---
+++ CalculatorWithFunctions/Program.cs
@@ -0,0 +1,37 @@
+﻿int a = 15;
+int b = 3;
+
+Add(a, b);
+Add(10, 100);
+Add(9754, 78);
+
+Multiply(a, b);
+Multiply(10, 100);
+Multiply(38, 45);
+
+Substract(a, b);
+Substract(10, 100);
+Substract(25, 5);
+
+Divide(a, b);
+Divide(100, 10);
+Divide(25, 5);
+static void Add(int x, int y)
+{
+    Console.WriteLine($"{x} + {y} = {x + y}");
+}
+
+static void Multiply (int x, int y)
+{
+    Console.WriteLine ($"{x} * {y} = {x * y}");
+}
+
+static void Substract(int x, int y)
+{
+    Console.WriteLine ($"{x} - {y} = {x - y}");
+}
+
+static void Divide(int x, int y)
+{
+    Console.WriteLine($"{x} / {y} = {x / y}");
+}
---
+++ Week7Functions/Program.cs
@@ -0,0 +1,23 @@
+﻿string hello = "Hello, world!";
+string panic = "Don't panic!";
+
+PrintStringFiveTimes(hello);
+PrintStringFiveTimes(panic);
+PrintStringFiveTimes("Roman");
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine(hello);
+//}
+//for (int i = 0;i < 5; i++)
+//{
+//    Console.WriteLine(panic);
+//}
+static void PrintStringFiveTimes(string someString)
+{
+    for (int i = 0; i < 5; i++)
+    {
+
+        Console.WriteLine(someString);
+    }
+
+}
